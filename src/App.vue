08708721219f7d362c0e70d1aaf5eb92f9ca8ff4
ZoeJi <zoe.q.ji@gmail.com>
2021-11-15 12:18:37 -0500

    update app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
+:root {
+  --base-border: 1px solid var(--border-color-base);
+  --border-color-base: #dcdfe6;
+  --box-shadow-basic: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
